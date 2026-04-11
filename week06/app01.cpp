1c85fec0216b18b450d212e04100d164e773ac7e
inhadeepblue <deepblue@inha.ac.kr>
2026-04-11 09:58:19 +0900

    week06 : inheritance vii

--- week06/app01.cpp
+++ week06/app01.cpp
@@ -2,20 +2,22 @@
 #include <string>
 using namespace std;
 
+//class Pokemon final
 class Pokemon
 {
     //private:
 protected:
+    string name;
     int level;
     int hp;
 public:
-    Pokemon() : level(1), hp(10)
+    Pokemon() : level(1), hp(10), name("nameless")
     {
     }
-    Pokemon(int level, int hp) :level(level), hp(hp)
+    Pokemon(int level, int hp, string name) :level(level), hp(hp), name(name)
     {
     }
-    Pokemon(const Pokemon& pokemon) :level(pokemon.level), hp(pokemon.hp)
+    Pokemon(const Pokemon& pokemon) :level(pokemon.level), hp(pokemon.hp), name(pokemon.name)
     {
     }
     void set(int level, int hp) {
@@ -27,6 +29,9 @@ public:
     }
     int getHp() const {
         return hp;
+    }
+    string getName() const {
+        return name;
     }
     void attack() {
         cout << "일반 공격\n";
@@ -44,11 +49,11 @@ public:
     Pikachu() : Pokemon()  // invocation
     {
     }
-    Pikachu(int level, int hp, string type) : Pokemon(level, hp), type(type)  // invocation
+    Pikachu(int level, int hp, string name, string type) : Pokemon(level, hp, name), type(type)  // invocation
     {
     }
     Pikachu(const Pikachu& pikachu)// :Pokemon(pikachu)  // invocation
-        :Pokemon(pikachu.level, pikachu.hp)  // invocation
+        :Pokemon(pikachu.level, pikachu.hp, pikachu.name)  // invocation
     {
     }
     void electricAttack(double attackPoint) {
@@ -66,18 +71,57 @@ public:
     }
 };
 
+class Squirtle : public Pokemon
+{
+private:
+    string type;
+public:
+    Squirtle() : Pokemon()
+    {
+    }
+    Squirtle(int level, int hp, string name, string type) : Pokemon(level, hp, name), type(type)
+    {
+    }
+    Squirtle(const Squirtle& squirtle)
+        :Pokemon(squirtle.level, squirtle.hp, squirtle.name)
+    {
+    }
+    void hydroAttack(double attackPoint) {
+        cout << attackPoint << "만 물대포 공격\n";
+    }
+    void hydroAttack(double attackPoint, Pokemon targetPokemon) {
+        cout << attackPoint << "만 물대포 공격을 " << targetPokemon.getName() << "에게 시전합니다\n";
+    }
+    void attack() {
+        cout << "물속성 일반 공격\n";
+    }
+    void set(int level, int hp, string type) {
+        Pokemon::set(level, hp); // delegation
+        this->type = type;
+    }
+    string getType() const {
+        return type;
+    }
+};
+
 int main() {
     Pokemon pokemon;
     pokemon.set(1, 5);
     cout << "포켓몬 레벨 : " << pokemon.getLevel();
     cout << endl << endl;
 
-    Pikachu pikachu;
-    pikachu.set(20, 200, "전기");
+    Squirtle squirtle(1, 7, "우리꼬부기", "물");
+    squirtle.hydroAttack(75);
+
+
+    Pikachu pikachu(20, 200, "야생의 피카츄", "전기");
+    //pikachu.set(20, 200, "전기");
     pikachu.electricAttack(100000);
     cout << "포켓몬 레벨 : " << pikachu.getLevel() << endl;
     pikachu.attack();
     //pikachu.attack(50);
     cout << pikachu.getType() << '\n';
+
+    squirtle.hydroAttack(3000, pikachu);
 	return 0;
 }
