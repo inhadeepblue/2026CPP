081adb554c1c23d9f05f87471360663afde48045
inhadeepblue <deepblue@inha.ac.kr>
2026-05-02 11:20:37 +0900

    week09 : polymorphism viii

--- week09/app02.cpp
+++ week09/app02.cpp
@@ -1,42 +1,42 @@
-#include <iostream>
-#include <string>
-#include <typeinfo> 
-using namespace std;
-
-class Animal { 
-public:
-	virtual void makeSound() { 
-		cout << "µ¿¹°ÀÌ ¿ïÀ½¼Ò¸®¸¦ ³À´Ï´Ù\n"; 
-	}
-};
-class Dog : public Animal {
-public:
-	void makeSound() {
-		cout << "¸Û¸Û!\n";
-	}
-};
-class Cat : public Animal {
-public:
-	void makeSound() {
-		cout << "³Ä¿Ë~\n";
-	}
-};
-
-int main()
-{
-	Animal* animal = new Dog();  // upcast
-	animal->makeSound();
-
-	//Cat* cat = (Cat*)animal; // downcast, old style (c style)  // danger
-	Cat* cat = dynamic_cast<Cat*>(animal); // downcast, modern style (c++ 11)
-	cout << animal << '\n';
-	cout << cat << '\n';
-
-	//dog->makeSound();
-	cat->makeSound();
-	
-	delete animal;
-	animal = nullptr;
-
-	return 0;
-}
+//#include <iostream>
+//#include <string>
+//#include <typeinfo> 
+//using namespace std;
+//
+//class Animal { 
+//public:
+//	virtual void makeSound() { 
+//		cout << "µ¿¹°ÀÌ ¿ïÀ½¼Ò¸®¸¦ ³À´Ï´Ù\n"; 
+//	}
+//};
+//class Dog : public Animal {
+//public:
+//	void makeSound() {
+//		cout << "¸Û¸Û!\n";
+//	}
+//};
+//class Cat : public Animal {
+//public:
+//	void makeSound() {
+//		cout << "³Ä¿Ë~\n";
+//	}
+//};
+//
+//int main()
+//{
+//	Animal* animal = new Dog();  // upcast
+//	animal->makeSound();
+//
+//	//Cat* cat = (Cat*)animal; // downcast, old style (c style)  // danger
+//	Cat* cat = dynamic_cast<Cat*>(animal); // downcast, modern style (c++ 11)
+//	cout << animal << '\n';
+//	cout << cat << '\n';
+//
+//	//dog->makeSound();
+//	cat->makeSound();
+//	
+//	delete animal;
+//	animal = nullptr;
+//
+//	return 0;
+//}
