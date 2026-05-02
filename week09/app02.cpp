e10f801aad340d0ac914e049691aaa2e2a89bbde
inhadeepblue <deepblue@inha.ac.kr>
2026-05-02 10:45:23 +0900

    week09 : polymorphism vii

--- week09/app02.cpp
+++ week09/app02.cpp
@@ -27,14 +27,14 @@ int main()
 	Animal* animal = new Dog();  // upcast
 	animal->makeSound();
 
-	Cat* cat = (Cat*)animal; // downcast, old style (c style)
-	//Dog* dog = dynamic_cast<Dog*>(animal); // downcast, modern style (c++ 11)
-	//dog->makeSound();
-	cat->makeSound();
-	
+	//Cat* cat = (Cat*)animal; // downcast, old style (c style)  // danger
+	Cat* cat = dynamic_cast<Cat*>(animal); // downcast, modern style (c++ 11)
 	cout << animal << '\n';
 	cout << cat << '\n';
 
+	//dog->makeSound();
+	cat->makeSound();
+	
 	delete animal;
 	animal = nullptr;
 
