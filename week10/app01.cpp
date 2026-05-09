5684f93141e7735f80a8551527737abcc3172ad4
inhadeepblue <deepblue@inha.ac.kr>
2026-05-09 11:35:17 +0900

    week10 : string v

--- week10/app01.cpp
+++ week10/app01.cpp
@@ -7,15 +7,13 @@ int main()
 {
 	//string a = "ACE";
 	string a("ACE");
-	cout << a.size() << '\n';
-	cout << a.capacity() << '\n';
-	a = "12345";
-	cout << a.size() << '\n';
-	cout << a.capacity() << '\n';
-	a = "1234567890123456";
-	cout << a.size() << '\n';
-	cout << a.capacity() << '\n';
-
+	//string a("ACE", 2);
+	//string a(7, 'A');
+	string b(a);
+	cout << a << '\n';
+	cout << b << '\n';
+	cout << b[1] << '\n';
+	cout << b.at(1) << '\n';
 
 	return 0;
 }
