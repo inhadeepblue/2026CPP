0af8a5686bbb897dd175916bd234ab6115912f5d
inhadeepblue <deepblue@inha.ac.kr>
2026-05-09 10:06:38 +0900

    week10 : polymorphism x

--- week10/app01.cpp
+++ week10/app01.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
 #include <string>
 using namespace std;
-class UndergraduateStudent { 
+
+class Student {
 public:
+	int id = 1;
+};
+
+class UndergraduateStudent : public Student{ 
+public:
+	int id = 2;
 	void warn() {
 		cout << "학사경고\n";
 	}
 };
-class DormitoryStudent {
+class DormitoryStudent : public Student {
 public:
+	int id = 3;
 	void warn() {
 		cout << "벌점부여\n";
 	}
@@ -18,11 +26,17 @@ class UndergraduateDormitoryStudent
 	: public UndergraduateStudent, public DormitoryStudent 
 {
 public:
+	void test() {
+		cout << DormitoryStudent::id << '\n';
+		cout << UndergraduateStudent::id << '\n';
+		//cout << id << '\n';  // ambiguous 
+	}
 };
 int main()
 {
 	UndergraduateDormitoryStudent uds1;
 	//uds1.warn();  // ambiguous
 	uds1.DormitoryStudent::warn();
+	uds1.test();
 	return 0;
 }
