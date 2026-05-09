b696c0ae72cdae9390975278a6050782d2a88194
inhadeepblue <deepblue@inha.ac.kr>
2026-05-09 10:40:38 +0900

    week10 : string i

--- week10/app01.cpp
+++ week10/app01.cpp
@@ -1,40 +1,24 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 using namespace std;
 
-class Student {
-public:
-	int id = 1;
-};
-
-class UndergraduateStudent : virtual public Student{ 
-public:
-	void warn() {
-		cout << "학사경고\n";
-	}
-};
-class DormitoryStudent : virtual public Student {
-public:
-	void warn() {
-		cout << "벌점부여\n";
-	}
-};
-// multiple inheritance
-class UndergraduateDormitoryStudent 
-	: public UndergraduateStudent, public DormitoryStudent 
-{
-public:
-	void test() {
-		cout << DormitoryStudent::id << '\n';
-		cout << UndergraduateStudent::id << '\n';
-		cout << id << '\n';
-	}
-};
 int main()
 {
-	UndergraduateDormitoryStudent uds1;
-	//uds1.warn();  // ambiguous
-	uds1.DormitoryStudent::warn();
-	uds1.test();
+	//char a[] = { 'A', 'C', 'E', '\0' };	
+	//char a[] = { 'A', 'C', 'E'};
+	char a[] = { 'A', 0, 'C', 'E'};
+	//char a[] = { 'A', 'C', 'E', NULL};
+	//char a[] = { 'A', 'C', 'E', 0 };
+	//char a[] = "ACE";
+	//const char a[] = "ACE";
+	//char* b = "XYZ";
+	const char* b = "XYZ";
+
+	cout << a << '\n';
+	//a[0] = 'X';
+	//cout << a << '\n';
+	//b[0] = 'K';
+
 	return 0;
 }
