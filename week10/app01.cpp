01e26991ee273cdf8542a17e56268ec8538c15b9
inhadeepblue <deepblue@inha.ac.kr>
2026-05-09 11:18:19 +0900

    week10 : string iii

--- week10/app01.cpp
+++ week10/app01.cpp
@@ -3,20 +3,14 @@
 #include <cstring>
 using namespace std;
 
-int myStrlen(char* ps) {
-	int i = 0;
-
-	while (ps[i] != NULL) {
-		i++;
-	}
-	return i;
-}
 int main()
 {
 	char a[] = "ACE";
-	cout << a << '\n';
-	cout << strlen(a) << '\n';
-	cout << myStrlen(a) << '\n';
+	char b[] = "ACE";
+	string c = "ACE";
+	string d = "ACE";
+	cout << strcmp(a, b) << '\n';	
+	cout << (c == d) << '\n';
 
 	return 0;
 }
