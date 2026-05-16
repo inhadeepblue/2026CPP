3bd6c59e74ab926f5ae4301c459b4d5d9f1a92d2
inhadeepblue <deepblue@inha.ac.kr>
2026-05-16 10:35:36 +0900

    week11 : operator overloading i

--- week11/app02.cpp
+++ week11/app02.cpp
@@ -2,15 +2,31 @@
 #include <iostream>
 using namespace std;
 
+class Complex {
+private:
+    int real;
+    int imaginary;
+public:
+    Complex(int real, int imaginary) : real(real), imaginary(imaginary) {
+    }
+    int getReal() const {
+        return real;
+    }
+    int getImaginary() const {
+        return imaginary;
+    }
+    Complex operator+(const Complex& right) {
+        int r = this->real + right.real;
+        int i = this->imaginary + right.imaginary;
+        return Complex(r, i);
+    }
+};
 int main()
-{
-    string a("123");
-    string b = "123";
-    
-    cout << boolalpha;
-    cout << (a < b) << '\n';
-    cout << noboolalpha;
-    cout << (a == b) << '\n';
-      
+{  
+    Complex c1(9, 2), c2(3, 5);
+    cout << c2.getImaginary() << '\n';
+    Complex c3 = c1 + c2;
+    cout << c3.getReal() << '\n';
+    cout << c3.getImaginary() << '\n';
     return 0;
 }
