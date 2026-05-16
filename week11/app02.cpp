9c6c9a36bb96e663e6e6a87e33269b4797ecbf09
inhadeepblue <deepblue@inha.ac.kr>
2026-05-16 10:19:27 +0900

    week11 : string vii

--- week11/app02.cpp
+++ week11/app02.cpp
@@ -2,25 +2,15 @@
 #include <iostream>
 using namespace std;
 
-string reverse(const string& original)
-{
-    string temp(original);
-    int size = original.size();
-    int i = 0;
-    while (i < size)
-    {
-        temp[i] = original[size - 1 - i];
-        i++;
-    }
-    temp[i] = '\0';
-    return temp;
-}
 int main()
 {
-    string original, copy;
-    getline(cin, original);
-    cout << original << endl;
-    copy = reverse(original);
-    cout << copy;
+    string a("123");
+    string b = "123";
+    
+    cout << boolalpha;
+    cout << (a < b) << '\n';
+    cout << noboolalpha;
+    cout << (a == b) << '\n';
+      
     return 0;
 }
