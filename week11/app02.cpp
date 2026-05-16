9dc20a0c6e69c606f8b8023e420079103ece473a
inhadeepblue <deepblue@inha.ac.kr>
2026-05-16 10:44:20 +0900

    week11 : operator overloading iv

--- week11/app02.cpp
+++ week11/app02.cpp
@@ -15,22 +15,22 @@ public:
     int getImaginary() const {
         return imaginary;
     }
-    Complex operator+(const Complex& right) {
-        int r = this->real + right.real;
-        int i = this->imaginary + right.imaginary;
-        return Complex(r, i);
-    }
+    //Complex operator+(const Complex& right) {
+    //    int r = this->real + right.real;
+    //    int i = this->imaginary + right.imaginary;
+    //    return Complex(r, i);
+    //}
 };
-//Complex operator+(const Complex& left, const Complex& right) {
-//    int r = left.getReal() + right.getReal();
-//    int i = left.getImaginary() + right.getImaginary();
-//    return Complex(r, i);
-//}
+Complex operator+(const Complex& left, const Complex& right) {
+    int r = left.getReal() + right.getReal();
+    int i = left.getImaginary() + right.getImaginary();
+    return Complex(r, i);
+}
 int main()
 {  
     Complex c1(9, 2), c2(3, 5);
     cout << c2.getImaginary() << '\n';
-    Complex c3 = c1.operator+(c2); //Complex c3 = c1 + c2;
+    Complex c3 = operator+(c1, c2); //Complex c3 = c1 + c2;
     cout << c3.getReal() << '\n';
     cout << c3.getImaginary() << '\n';
     return 0;
