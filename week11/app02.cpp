e7199ab303c8456ad67cefe48746f6e928480365
inhadeepblue <deepblue@inha.ac.kr>
2026-05-16 11:09:40 +0900

    week11 : operator overloading v

--- week11/app02.cpp
+++ week11/app02.cpp
@@ -20,6 +20,11 @@ public:
     //    int i = this->imaginary + right.imaginary;
     //    return Complex(r, i);
     //}
+    Complex operator++(int) {  // postfix
+        Complex backup(this->real, this->imaginary);
+        this->real = this->real + 1;
+        return backup;
+    }
 };
 Complex operator+(const Complex& left, const Complex& right) {
     int r = left.getReal() + right.getReal();
@@ -33,5 +38,10 @@ int main()
     Complex c3 = operator+(c1, c2); //Complex c3 = c1 + c2;
     cout << c3.getReal() << '\n';
     cout << c3.getImaginary() << '\n';
+    Complex c4 = c3++;
+    cout << c4.getReal() << '\n';
+    cout << c4.getImaginary() << '\n';
+    cout << c3.getReal() << '\n';
+
     return 0;
 }
