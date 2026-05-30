77580b5bee7ac32abad23ee2c83bbfe7cabce849
inhadeepblue <deepblue@inha.ac.kr>
2026-05-30 09:32:47 +0900

    week13 : exception handling v

--- week13/app01.cpp
+++ week13/app01.cpp
@@ -5,7 +5,10 @@ int main()
     DynamicArray da1(5);
 
     da1.setAt(0, 54);
-    cout << da1.getAt(0);
+    cout << da1.getAt(0) << '\n';
+    cout << da1.getAt(7) << '\n';
+    da1.setAt(6, -19);
+    cout << da1.getAt(6) << '\n';
 
     return 0;
 }
