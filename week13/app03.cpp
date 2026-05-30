0e5febace3900eb1305c6fd66c7c728a0475111a
inhadeepblue <deepblue@inha.ac.kr>
2026-05-30 11:50:11 +0900

    week13 : template iii

--- week13/app03.cpp
+++ week13/app03.cpp
@@ -1,19 +1,87 @@
-#include <iostream>
+﻿#include <iostream>
+#include <cassert>
 using namespace std;
 
 template <typename T>
-T bigger(T first, T second)
+class Stack
 {
-	if (first > second)
-		return first;
-	return second;
+private:
+    T* ptr;
+    int capacity;
+    int size;
+public:
+    Stack(int capacity);
+    ~Stack();
+    void push(const T& element);
+    T pop();
+};
+template<typename T>
+Stack<T>::Stack(int cap)
+    : capacity(cap), size(0)
+{
+    ptr = new T[capacity];
 }
+template<typename T>
+Stack<T>::~Stack()
+{
+    delete[] ptr;
+    ptr = nullptr;
+}
+template<typename T>
+void Stack<T>::push(const T& element)
+{
+    if (size < capacity)
+    {
+        ptr[size] = element;
+        size++;
+    }
+    else
+    {
+        cout << "스택이 꽉 찼습니다." << endl;
+        assert(false);
+    }
+}
+template<typename T>
+T Stack<T>::pop()
+{
+    if (size > 0)
+    {
+        T temp = ptr[size - 1];
+        size--;
+        return temp;
+    }
+    else
+    {
+        cout << "스택이 비어있습니다." << endl;
+        assert(false);
+    }
+}
+class Pokemon {
+public:
+    int hp;
+    Pokemon() :hp(1) {};
+    Pokemon(int hp) : hp(hp) {};
+};
+ostream& operator<<(ostream& o, const Pokemon& pokemon) {
+    o << pokemon.hp << '\n';
+    return o;
+}
+typedef Stack<int> iStack;
+typedef Stack<Pokemon> pokemonStack;
 int main()
 {
-	int i1 = 1, i2 = 2;
-	double d3 = 3.7;	
-	cout << bigger(i1, i2) << endl;
-	//cout << bigger(i2, d3) << endl;
-	cout << bigger<int>(i2, d3) << endl;  // 2.0
-	return 0;
+    Pokemon pikachu(100);
+    Pokemon squirtle(120);
+    iStack stack(3);
+    pokemonStack jiwoo(4);
+    jiwoo.push(squirtle);
+    jiwoo.push(pikachu);
+    stack.push(7);
+    stack.push(3);
+    cout << stack.pop() << '\n';
+    //cout << stack.pop() << '\n';
+    cout << stack.pop() << '\n';
+    cout << stack.pop() << '\n';
+    cout << jiwoo.pop() << '\n';
+    return 0;
 }
