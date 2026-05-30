ee7af53c9ed7a341430a215c6f501d5e7b2fe0bd
inhadeepblue <deepblue@inha.ac.kr>
2026-05-30 09:24:38 +0900

    week13 : exception handling iii

--- week13/dynamicarray.cpp
+++ week13/dynamicarray.cpp
@@ -1,12 +1,17 @@
-#pragma once
-#include <iostream>
-#include <string>
-using namespace std;
-class DynamicArray {
-	int size;
-	int* ptr;
-public:
-	DynamicArray();
-	DynamicArray(int size);
-	~DynamicArray();
-};
+#include "dynamicarray.h"
+
+DynamicArray::DynamicArray() : size(1)
+{
+	ptr = new int[1];
+}
+DynamicArray::DynamicArray(int size) : size(size)
+{
+	ptr = new int[size];
+	//this->size = size;
+	//(*this).size = size;
+}
+DynamicArray::~DynamicArray()
+{
+	delete[] ptr;
+}
+
